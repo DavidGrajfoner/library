da53589d86b6f9c5285046a6c3fdf2a1ec727913
DavidGrajfoner <davidgrajfoner@gmail.com>
2024-10-16 00:54:30 +0200

    Db konfiguracija premaknjena v .env

--- database/database.go
+++ database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"fmt"
 	"library/models"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,13 +14,25 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-    dsn := "host=db user=user password=password dbname=library port=5432 sslmode=disable"
-    var err error
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    host := os.Getenv("DB_HOST")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
+    sslmode := os.Getenv("DB_SSLMODE")
+
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", 
+        host, user, password, dbname, port, sslmode)
+
     DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Failed to connect to the database:", err)
     }
-
-    // Migracija modelov
+    
     DB.AutoMigrate(&models.User{}, &models.Book{}, &models.Borrow{})
 }
